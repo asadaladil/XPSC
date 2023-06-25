50bd9fb0c5f7a5b56181a7aaed8a4e974e5e5fcd
MD. ASAD-AL-ADIL <adilasadals@gmail.com>
2023-06-26 01:51:08 +0600

    week 4 day 3 update

--- Practice Problem/Week 04/Day 3/train and query.cpp
+++ Practice Problem/Week 04/Day 3/train and query.cpp
@@ -5,29 +5,33 @@ int main()
 {
     int t;
     cin>>t;
-    ll n=1e9;
     while(t--)
     {
         ll a,b;
         cin>>a>>b;
-        ll A[n];
+        map<ll,ll>mp1,mp2;
         for(int i=1;i<=a;i++)
         {
             ll x;
             cin>>x;
-            A[x]=i;
+            mp1[x]==0?mp1[x]=i:mp2[x]=i;
         }
         while(b--)
         {
             ll x,y;
             cin>>x>>y;
-            if(x<y&&x!=0&&y!=0)
+            if(mp1[x]==0||mp1[y]==0)
+            {
+                cout<<"NO\n";
+                continue;
+            }
+            if(mp1[x]<mp1[y]||mp1[x]<mp2[y])
             {
                 cout<<"YES\n";
             }
             else
             {
-                cout<< "NO\n";
+                cout<<"NO\n";
             }
         }
     }
