471c158400e7f458d4b93eae9f8defbc375256c7
MD. ASAD-AL-ADIL <adilasadals@gmail.com>
2023-07-05 02:13:09 +0600

    week 4 day 9 update

---
+++ Practice Problem/Week 04/Day 9/2 round dances.cpp
@@ -0,0 +1,37 @@
+/*
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||     Rajshahi Unversity of Engineering & Technology      ||
+||                                                         ||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||                   MD. ASAD-AL-ADIL                      ||
+||                                                         ||
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define str string
+#define ev(x) x%2==0
+#define od(x) x%2==1
+#define P_B push_back
+#define p_b pop_back
+#define P_F push_front
+#define p_f pop_front
+#define pi 3.141592654
+ll f(int x){if(x==1||x==0){return 1;}return x*f(x-1);}
+#define __lcm(x,y) (x*y)/__gcd(x,y)
+#define A_sort(x) sort(x.begin(),x.end())
+#define D_sort(x) sort(x.rbegin(),x.rend())
+#define reverse(x) reverse(x.begin(),x.end())
+#define endl "\n"
+int main()
+{
+    int n,a;cin>>n;a=pow(n/2,2)*2;
+    cout<<f(n)/a;
+
+}
+
+
+
