283cb461b48f3f58462c8468f8beb77389bd92e3
MD. ASAD-AL-ADIL <adilasadals@gmail.com>
2023-07-18 15:50:46 +0600

    week 6 day 5 update

---
+++ Practice Problem/Week 06/Day 5/DDMM.cpp
@@ -0,0 +1,59 @@
+/*
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||     Rajshahi Unversity of Engineering & Technology      ||
+||                                                         ||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||                   MD. ASAD-AL-ADIL                      ||
+||                                                         ||
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define _easy_ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define str string
+#define ev(x) x%2==0
+#define od(x) x%2==1
+#define mod0(x,y) x%y==0 
+#define P_B push_back
+#define p_b pop_back
+#define P_F push_front
+#define p_f pop_front
+#define pi 3.141592654
+int f(int x){if(x==1){return 1;}return x*f(x-1);}
+#define __lcm(x,y) (x*y)/__gcd(x,y)
+#define A_sort(x) sort(x.begin(),x.end())
+#define D_sort(x) sort(x.rbegin(),x.rend())
+#define reverse(x) reverse(x.begin(),x.end())
+#define yes_or_no cout<<"YES\n":cout<<"NO\n"
+#define yess cout<<"YES\n"
+#define noo cout<<"NO\n"
+#define endl "\n"
+#define newl cout<<endl
+int main()
+{
+    _easy_
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        str r;
+        cin>>r;
+        int a=(r[0]-'0')*10+(r[1]-'0');
+        int b=(r[3]-'0')*10+(r[4]-'0');
+        if(a<=12&&b<=12)
+        {
+            cout<<"BOTH\n";
+        }
+        else if(b<=12)
+        {
+            cout<<"DD/MM/YYYY\n";
+        }
+        else
+        {
+            cout<<"MM/DD/YYYY\n";
+        }
+    }
+}
