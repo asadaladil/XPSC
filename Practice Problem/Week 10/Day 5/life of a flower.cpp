fae0d589d7fd366bcc9b3c5a3929c9b0cdc7b4cc
MD. ASAD-AL-ADIL <adilasadals@gmail.com>
2023-08-15 12:51:05 +0600

    week 10 day 5 update

---
+++ Practice Problem/Week 10/Day 5/life of a flower.cpp
@@ -0,0 +1,88 @@
+/*
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||     Rajshahi Unversity of Engineering & Technology      ||
+||                                                         ||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||                   MD. ASAD-AL-ADIL                      ||
+||                                                         ||
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define _easy_ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define str string
+#define ev(x) x%2==0
+#define od(x) x%2==1
+#define mod0(x,y) x%y==0 
+#define P_B push_back
+#define p_b pop_back
+#define P_F push_front
+#define p_f pop_front
+#define pi 3.141592654
+int f(int x){if(x==1){return 1;}return x*f(x-1);}
+#define __lcm(x,y) (x*y)/__gcd(x,y)
+#define A_sort(x) sort(x.begin(),x.end())
+#define D_sort(x) sort(x.rbegin(),x.rend())
+#define reverse(x) reverse(x.begin(),x.end())
+#define vary(A,x,y,n) transform(A.begin()+x,A.begin()+y,A.begin()+x,[](int a){return a+n;})
+#define yes_or_no cout<<"YES\n":cout<<"NO\n"
+#define yess cout<<"YES\n"
+#define noo cout<<"NO\n"
+#define endl "\n"
+#define newl cout<<endl
+int main()
+{
+    _easy_
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>A(n);
+        bool flag=true;
+        for(int i=0;i<n;i++)
+        {
+            cin>>A[i];
+        }
+        int x=1,y=0,z=0;
+        for(int i=0;i<n;i++)
+        {
+            if(A[i]==1)
+            {
+                y++;
+                z=0;
+                if(y==1)
+                {
+                    x++;
+                }
+                else
+                {
+                    x+=5;
+                }
+            }
+            else
+            {
+                z++;y=0;
+                if(z==2)
+                {
+                    flag=false;
+                    break;;
+                }
+            }
+        }
+        if(flag)
+        {
+            cout<<x;
+        }
+        else
+        {
+            cout<<-1;
+        }
+        newl;
+    }
+    
+}
