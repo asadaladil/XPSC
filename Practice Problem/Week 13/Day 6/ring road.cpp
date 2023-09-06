9c9a59e3f83ea7c427e239b802e35a8cfc9d615a
MD. ASAD-AL-ADIL <adilasadals@gmail.com>
2023-09-06 23:27:16 +0600

    week 13 day 6 update

---
+++ Practice Problem/Week 13/Day 6/ring road.cpp
@@ -0,0 +1,62 @@
+/*
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||     Rajshahi Unversity of Engineering & Technology      ||
+||                                                         ||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||                   MD. ASAD-AL-ADIL                      ||
+||                                                         ||
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define _easy_ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define str string
+#define ev(x) x%2==0
+#define od(x) x%2==1
+#define mod0(x,y) x%y==0 
+#define P_B push_back
+#define p_b pop_back
+#define P_F push_front
+#define p_f pop_front
+#define pi 3.141592654
+int f(int x){if(x==1){return 1;}return x*f(x-1);}
+#define __lcm(x,y) (x*y)/__gcd(x,y)
+#define A_sort(x) sort(x.begin(),x.end())
+#define D_sort(x) sort(x.rbegin(),x.rend())
+#define Reverse(x) reverse(x.begin(),x.end())
+#define Vary(A,x,y,n) transform(A.begin()+x,A.begin()+y,A.begin()+x,[](int a){return a+n;})
+#define Vary_perm(x) next_permutation(x.begin(),x.end())
+#define yes_or_no cout<<"YES\n":cout<<"NO\n"
+#define yess cout<<"YES\n"
+#define noo cout<<"NO\n"
+#define endl "\n"
+#define newl cout<<endl
+int main()
+{
+    _easy_
+    unordered_map<int,bool>mp1,mp2;
+    int n;
+    cin>>n;
+    int ans=0,res=0;
+    while(n--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(mp1[a]||mp2[b])
+        {
+            ans+=c;
+            mp1[b]=true;
+            mp2[a]=true;
+        }
+        else
+        {
+            mp1[a]=true;
+            mp2[b]=true;
+        }
+        res+=c;
+    }
+    cout<<min(ans,res-ans);
+}
