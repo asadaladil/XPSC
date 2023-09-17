8f2b8816018a165c6c0cae3e0076e850cf540a28
MD. ASAD-AL-ADIL <adilasadals@gmail.com>
2023-09-17 23:36:38 +0600

    week 15 day 3 update

---
+++ Practice Problem/Week 15/Day 3/green gold.cpp
@@ -0,0 +1,65 @@
+/*
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||     Rajshahi Unversity of Engineering & Technology      ||
+||                                                         ||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||                                                         ||
+||                   MD. ASAD-AL-ADIL                      ||
+||                                                         ||
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define _easy_ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define str string
+#define ev(x) x%2==0
+#define od(x) x%2==1
+#define mod0(x,y) x%y==0 
+#define P_B push_back
+#define p_b pop_back
+#define P_F push_front
+#define p_f pop_front
+#define pi 3.141592654
+int f(int x){if(x==1){return 1;}return x*f(x-1);}
+#define __lcm(x,y) (x*y)/__gcd(x,y)
+#define A_sort(x) sort(x.begin(),x.end())
+#define D_sort(x) sort(x.rbegin(),x.rend())
+#define Reverse(x) reverse(x.begin(),x.end())
+#define Vary(A,x,y,n) transform(A.begin()+x,A.begin()+y,A.begin()+x,[](int a){return a+n;})
+#define Vary_perm(x) next_permutation(x.begin(),x.end())
+#define yes_or_no cout<<"YES\n":cout<<"NO\n"
+#define yess cout<<"YES\n"
+#define noo cout<<"NO\n"
+#define endl "\n"
+#define newl cout<<endl
+int main()
+{
+    _easy_
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>B(n);
+        vector<pair<int,int>>A;
+        for(int i=0;i<n;i++)
+        {
+            int a;cin>>a;
+            A.P_B({a,i});
+        }
+        A_sort(A);
+        for(int i=0;i<n;i++)
+        {
+            B[A[i].second]=n-i;
+        }
+        for(int i:B)
+        {
+
+            cout<<i<<" ";
+        }
+        newl;
+    }
+}
